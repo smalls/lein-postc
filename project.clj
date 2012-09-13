2384863737e8d4e48e57d5aa298cd36d7bbfa351
smalls <small@cybertronic.com>
2012-09-13 15:12:10 -0700

    update project

--- project.clj
+++ project.clj
@@ -1,12 +1,10 @@
-(defproject smallblog "1.0.0-SNAPSHOT"
-            :description    "smallblog"
+(defproject smallblog-static "1.0.0-SNAPSHOT"
+            :description    "smallblog-static"
             :dependencies   [[org.clojure/clojure "1.4.0"]
                              [clj-time "0.4.3"]
                              [enlive "1.0.0"]
                              [rhino/js "1.7R2"]
                              [com.thebuzzmedia/imgscalr-lib "3.2"]
                              [net.java.dev.jets3t/jets3t "0.9.0"]]
-            :dev-dependencies   [[vimclojure/server "2.2.0"]
-                                 [org.clojars.autre/lein-vimclojure "1.0.0"]]
             :repositories   {"The Buzz Media Maven Repository" ; imgscalr
                              "http://maven.thebuzzmedia.com"})
